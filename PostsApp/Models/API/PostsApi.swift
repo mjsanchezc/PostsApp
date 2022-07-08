a21041d5af10fa2ed03230b8d18eb78a6af4d3c7
Maria Jose Sanchez Cairazco <mariajose.sanchezcairazco@gmail.com>
2022-07-07 22:24:13 -0500

    Revert "Remove the now ignored directory "some-directory""
    This reverts commit c00084af751dcb173a26d6bfd1fdeb774ff0feab.

---
+++ PostsApp/Models/API/PostsApi.swift
@@ -0,0 +1,12 @@
+//
+//  PostsApi.swift
+//  PostsApp
+//
+//  Created by Maria Jose Sanchez Cairazco on 7/07/22.
+//
+
+import Foundation
+
+func makeRequest() {
+    let url = URL(string: "http://www.stackoverflow.com")!
+}
